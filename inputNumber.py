484fe07226f77de8faf55022cf3ad47b9d2ae8c9
Magnus Therkelsen <Macnetic@users.noreply.github.com>
2017-06-12 14:06:44 +0200

    Include menu chooser

---
+++ inputNumber.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  3 21:53:42 2017
+
+@author: megam
+"""
+
+def inputNumber(prompt):
+    # INPUTNUMBER Prompts user to input a number
+    #
+    # Usage: num = inputNumber(prompt) Displays prompt and asks user to input a
+    # number. Repeats until user inputs a valid number.
+    #
+    # Author: Mikkel N. Schmidt, mnsc@dtu.dk, 2015
+    while True:
+        try:
+            num = float(input(prompt))
+            break
+        except ValueError:
+            pass
+        
+    return num
