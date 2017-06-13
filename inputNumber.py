fc36903c07b317f83d278bef0a89dc0bdf807be6
Magnus Therkelsen <Macnetic@users.noreply.github.com>
2017-06-13 14:17:02 +0200

    Refactor inputNumber
    Now you can input both integers and floating-point numbers

--- inputNumber.py
+++ inputNumber.py
@@ -5,7 +5,21 @@ Created on Sat Jun  3 21:53:42 2017
 @author: megam
 """
 
-def inputNumber(prompt):
+def inputFloat(prompt):
+    """
+    Prompts the user to input a number and returns it as a float.
+    Repeats until user inputs a valid number.
+    
+    Parameters
+    ----------
+    prompt : str
+        User prompt
+    
+    Return
+    ------
+    num : float
+        User input float
+    """
     # INPUTNUMBER Prompts user to input a number
     #
     # Usage: num = inputNumber(prompt) Displays prompt and asks user to input a
@@ -20,3 +34,20 @@ def inputNumber(prompt):
             pass
         
     return num
+
+def inputInt(prompt):
+    """
+    Prompts the user to input a number and returns it as an integer.
+    Repeats until user inputs a valid number.
+    
+    Parameters
+    ----------
+    prompt : str
+        User prompt
+    
+    Return
+    ------
+    num : int
+        User input integer
+    """
+    return int(inputFloat(prompt))
