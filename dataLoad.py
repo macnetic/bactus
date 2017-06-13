375ad200f426813287aea7679325e98d4288dda5
Carl Emil <carlemilelling@hotmail.com>
2017-06-13 10:57:39 +0200

    Update to Data Load
    almost there!

--- dataLoad.py
+++ dataLoad.py
@@ -18,18 +18,30 @@ def dataLoad(filename):
     ------
     Returns data as N x 3 matrix
     """
+    # Text file is loaded as a CSV file with spaces as separators
     data = np.array(pd.read_csv(filename,sep=" "))
-    dataErrorFree = np.zeros(3)
-    dataErrors = np.array([])
+    dataErrorFree = np.array([])
+    dataErrorTemperature = np.zeros(3)
+    dataErrorGrowth = np.array([])
+    dataErrorBacteria = np.array([])
     for i in range(len(data[:,0])):
         # Does temperature match delimiters? 
         temperatureInRange = 10 <= data[i,0] <= 60
         # Does growth rate match delimiters?
         growthInRange = 0 <= data[i,1]
         # Does bacteria type match delimiters?   
-        bacteriaInRange = 1 <= data[i,2] <=4
+        bacteriaInRange = 1 <= data[i,2] <= 4
         if (temperatureInRange and growthInRange and bacteriaInRange): 
-            np.append(dataErrorFree,data[i,:],axis=0)
-        print()
+            dataErrorFree = np.append(dataErrorFree, data[i,:])
+        elif temperatureInRange == False:
+            dataErrorTemperature = np.vstack((dataErrorTemperature,data[i,:]))
+        elif growthInRange == False:
+            dataErrorGrowth = np.append(dataErrorGrowth,data[i,:],axis=0)
+        elif bacteriaInRange == False:
+            dataErrorBacteria = np.append(dataErrorBacteria,data[i,:],axis=0)
         
+    print(dataErrorFree)
+    print(dataErrorTemperature)
+    print(dataErrorGrowth)
+    print(dataErrorBacteria)
 dataLoad("test.txt")
