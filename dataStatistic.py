2e352c9c12fcca59483b99671c49c008a32d7e95
Carl Emil <carlemilelling@hotmail.com>
2017-06-13 12:15:50 +0200

    Data statistic and data filtering documents setup

---
+++ dataStatistic.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 12:14:56 2017
+
+@author: carle
+"""
+
+def dataStatistics(data,statistic):
+    
+    return(result)
---
+++ filterData.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 14:12:10 2017
+
+@author: Magnus Oksbøl Therkelsen & Carl Emil Elling
+"""
+
+def filterData(data):
+    
+    return data
