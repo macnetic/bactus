b1f1f2541f4bbbf8a1267262a5947ee0766393fc
Magnus Therkelsen <Macnetic@users.noreply.github.com>
2017-06-14 12:52:30 +0200

    Refactored number input functions

--- inputNumber.py
+++ inputNumber.py
@@ -43,20 +43,79 @@ def inputInt(prompt):
     Return
     ------
     num : int
-        User input integer
+        Input from user
     """
-    return int(inputFloat(prompt))
+    num = int(inputFloat(prompt))
+    return num
 
-def inputRange(prompt, sep=','):
+def inputSet(prompt, sep=','):
+    """
+    Prompts the user for a set of numbers and returns them, 
+    or None if user doesn't input a range.
+    
+    Parameters
+    ----------
+    prompt : str
+        User prompt
+    sep : str, optional
+        Separator between numbers
+    """
     while True:
         try:
             str_in = input(prompt)
-            if input == '':
-                # TODO
-                pass
-            f_range = tuple(float(num) for num in input(prompt).split(sep=sep))
+            if str_in == '':
+                # Indicates that no set has been selected.
+                num_set = None
+                break
+            
+            # Attempt to convert to tuple containing values
+            num_set = tuple(float(num) for num in str_in.split(sep=sep))
             break
         except ValueError:
+            print('Invalid input')
+        
+    return num_set
+
+def inputRange(prompt,  sep=','):
+    """
+    Prompts the user for a numerical range and returns range, 
+    or None if user doesn't input a range.
+    
+    Parameters
+    ----------
+    prompt : str
+        User prompt
+    sep : str, optional
+        Separator between numbers
+    """
+    while True:
+        f_range = inputSet(prompt, sep)
+        # A range is two numbers, so raise an error if it's not
+        if f_range == None:
+            break
+        elif len(f_range) != 2:
             print('Invalid range')
+            continue
+        
+        break
         
     return f_range
+
+def inputIntSet(prompt, sep=','):
+    """
+    Prompts the user for a set of integers and returns them, 
+    or None if user doesn't input a range.
+    
+    Parameters
+    ----------
+    prompt : str
+        User prompt
+    sep : str, optional
+        Separator between numbers
+    """
+    user_input = inputSet(prompt, sep)
+    if user_input == None:
+        return user_input
+    else:
+        int_set = tuple(int(e) for e in user_input)
+        return int_set
